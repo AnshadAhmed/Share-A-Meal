aecde2b2419bf5f4ac462d0da43f048d5722ff3f
Adnan <anadn200@gmail.com>
2025-02-11 15:27:06 +0530

    `Added Home page and corrected login/register link text`

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
   return (
     <>
     <Routes>
+      <Route path="/" element={<Home/>}></Route>
       <Route path="/login" element={<Login/>}></Route>
       <Route path="/register" element={<Register/>}></Route>
     </Routes>
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+// import './Home.css'
+
+
+
+function Home() {
+  return (
+    <>
+
+    <a href="/login"> LOG IN</a>   
+<a href="/register">REGISTER</a>
+ </>
+  )
+}
+
+export default Home
