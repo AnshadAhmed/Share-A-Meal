c0b2ba698ef68b949676c7dffd520869fd8ac379
Adnan <anadn200@gmail.com>
2025-02-17 11:23:30 +0530

    `Added navigation functionality to login and register pages, implemented JWT token generation and storage in login endpoint, and updated API routes to return JSON responses.`

--- server/app.js
+++ server/app.js
@@ -30,7 +30,12 @@ app.use(cors())
 
 app.post("/login", [
     body('email').isEmail().withMessage('Enter a valid email address'),
-    body('pwd').isLength({ min: 5 }).withMessage('Password must be at least 5 characters long')
+    body('pwd').isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters long')
+    .matches(/[0-9]/)
+    .withMessage('Password must contain at least one number')
+    .matches(/[!@#$%^&*]/)
+    .withMessage('Password must contain at least one special character')
 ], async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -74,7 +79,13 @@ app.post("/login", [
 app.post('/register', [
     body('username').notEmpty().withMessage('Username is required'),
     body('email').isEmail().withMessage('Enter a valid email address'),
-    body('pwd').isLength({ min: 5 }).withMessage('Password must be at least 5 characters long')
+    body('pwd')
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters long')
+    .matches(/[0-9]/)
+    .withMessage('Password must contain at least one number')
+    .matches(/[!@#$%^&*]/)
+    .withMessage('Password must contain at least one special character')
 ], async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
---
+++ server/routes/user.js
@@ -0,0 +1,18 @@
+// const express=require('express')
+// const router=express.Router()
+// const usercontrollers=require('../controllers/usercontrollers')
+
+
+
+
+
+
+
+
+// router.post('/login',usercontrollers.logincontroller)
+// router.post('/register',usercontrollers.registercontroller)
+
+
+
+
+// module.exports=router
