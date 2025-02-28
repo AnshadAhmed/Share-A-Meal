b5ab85c3b10b0d93297775086bed6f7d16b58776
Anshad <anshad1216@gmail.com>
2025-03-01 01:28:55 +0530

    Revert "MVC"
    This reverts commit 0da914f95a0d6d7fe23b00799fbdc32e6f36602a.

--- server/controllers/usercontrollers.js
+++ server/controllers/usercontrollers.js
@@ -1,3 +1,90 @@
+<<<<<<< Updated upstream
+// const bcrypt = require('bcryptjs')
+// const jwt = require('jsonwebtoken')
+// const User = require('../model/User')
+
+
+
+
+
+// const logincontroller = async (req, res) => {
+//     try {
+//         const { email, pwd } = req.body;
+//         const userdata = await User.findOne({ email: email });
+
+//         // console.log(userdata);
+
+
+//         const ispassword = await bcrypt.compare(pwd, userdata.pwd)
+
+
+//         if (ispassword) {
+
+//             const token = jwt.sign({ userId: userdata._id }, process.env.SECRET_KEY, { expiresIn: "1h" });
+//             console.log(`the jwt token= ${token}`);
+
+
+//             res.json({ msg: "Login Successfull", token })
+//         } else {
+//             res.json({ msg: "invalid Password" })
+
+//         }
+
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).send("Server error");
+
+//     }
+
+// }
+
+
+
+
+
+
+
+// const registercontroller = async (req, res) => {
+//     try {
+//         const { username, email, pwd } = req.body;
+
+//         //to check if the email alredy exists in DB
+
+//         const userdata = await User.findOne({ email: email });
+
+//         if (userdata) {
+//             return res.send("Email Already Exists");
+//         }
+
+//         //bcrypy hashing the password
+
+//         const salt = await bcrypt.genSalt(10)
+//         const hashedPassword = await bcrypt.hash(pwd, salt)
+
+
+//         //adding to DB
+
+//         const newUser = await User.create({ username, email, pwd: hashedPassword });
+//         await newUser.save();
+
+//         // console.log(username);
+//         // console.log(email);
+//         // console.log(pwd);
+
+//         res.status(201).send('User added successfully');
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send("Server error");
+
+//     }
+// }
+
+
+
+
+
+// module.exports = { logincontroller, registercontroller }
+=======
 const User = require('../models/User');
 
 exports.getUserProfile = async (req, res) => {
@@ -25,3 +112,4 @@ exports.editUserProfile = async (req, res) => {
         res.status(500).send("Server error");
     }
 };
+>>>>>>> Stashed changes
---
+++ server/routes/user.js
@@ -0,0 +1,20 @@
+// const express=require('express')
+// const router=express.Router()
+// const usercontrollers=require('../controllers/usercontrollers')
+// const {loginvalidation,registervalidation}=require('./middleware/validate')
+
+
+
+
+
+
+
+
+
+// router.post('/login',loginvalidation,usercontrollers.logincontroller)
+// router.post('/register',registervalidation,usercontrollers.registercontroller)
+
+
+
+
+// module.exports=router
